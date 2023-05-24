bef4e807d09a515b642a79096cb36baeed6c4c1b
ElisaCp <elisacampospousa@gmail.com>
2023-05-24 14:11:05 +0200

    Header responsive structure done

--- src/app/weather-app/shared-components/header/header.component.ts
+++ src/app/weather-app/shared-components/header/header.component.ts
@@ -13,10 +13,9 @@ export class HeaderComponent implements OnInit{
   }
   ngOnInit() {
   this.onResize();
-  console.log('isMenuOpen', this.isMenuOpen);
 }
 
-  toggleMenu() {
+  toggleMenuMobile() {
     this.isMenuOpen = !this.isMenuOpen;
 
     if(this.isMenuOpen) {
@@ -24,19 +23,14 @@ export class HeaderComponent implements OnInit{
     } else {
       this.renderer.removeClass(document.body, 'menu-on')
     }
-    console.log('isMenuOpen', this.isMenuOpen);
   }
-
+// TODO: mirar bien el hostlistener
   @HostListener('window:resize') onResize() {
-    console.log('ENTRO EN HOSTLISTENER Y ES MOBILE', this.isMobile)
     if (window.innerWidth < 769) { // 768px portrait
       this.isMobile = true;
       this.isMenuOpen = false;
-      console.log('MENOS 769, isMobile?', this.isMobile)
     } else {
       this.isMobile = false;
-      // TODO: ver si pongo aqui mejor que con click para el body
-      console.log('MAS 769, isMobile?', this.isMobile)
     }
   }
 }
